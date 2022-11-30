dc03e9250c291951d19ce5a3e33bb419f899fcc3
GVRemington <104083156+GVRemington@users.noreply.github.com>
2022-11-30 10:34:41 -0800

    initial

---
+++ routes/api.js
@@ -0,0 +1,42 @@
+var express = require('express');
+var router = express.Router();
+
+
+var data = [
+    { id: 0, name: "person1", username: "user1" },
+    { id: 1, name: "person2", username: "user2 "},
+    { id: 2, name: "person3", username: "user3 "}
+]
+
+router.get('/person', function(req, res, next){
+    res.status(200);
+    res.send(data);
+});
+
+router.get('/person/:id', function(req, res, next){
+    res.status(200);
+    res.send(data.filter(person => {
+        person.id === req.params.id
+    }));
+});
+
+router.post('/person', function(req, res, next){
+    res.status(200);
+    data.push(req.body);
+    res.send(req.body);
+});
+router.put('/person/:id', function(req, res, next){
+    res.status(200);
+
+    var index = data.indexOf(person => {
+        return person.id === req.params.id;
+    });
+});
+
+router.delete('/person/:id', function(requ, res, next){
+    res.status(200);
+    data[index] = data.filter(person => person.id != req.params.id);
+    res.send(data[index]);
+});
+
+module.exports = router;
